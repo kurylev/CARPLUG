86b495213e4a680b35f99562dee68eb22c53f76d
kau <kuryleva@mail.ru>
2011-08-04 23:51:47 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carplug::Application.config.secret_token = '5ca44beb92327f2bb3eb0e1ff49ba450d78612a8ae8199a2913b4d5a2f0033557d5856fa8f87ecbdea3d07adac127bca6d562781e0a56129ee45ef368e8e4bbd'
